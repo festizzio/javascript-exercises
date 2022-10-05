7c927e8c8faed8c14164af80937a16b88c9089f8
Sean <festizzio@gmail.com>
2022-10-05 06:59:12 -0700

    Add code for the calculator's functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sum = 0;
+  for(const num of array) {
+    sum += num;
+  }
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  let result = 1;
+  for(const num of array) {
+    result *= num;
+  }
 
+  return result;
 };
 
-const power = function() {
-	
+const power = function(num, exponent) {
+  let result = 1;
+  for(let i = 0; i < exponent; i++) {
+    result = result * num;
+  }
+
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(factorialNum) {
+	if(factorialNum === 0) {
+    return 1;
+  }
+
+  return factorial(factorialNum - 1) * factorialNum;
 };
 
 // Do not edit below this line

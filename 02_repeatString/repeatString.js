a9fc16bca8baf1fdea0cc6a6a3d9e981c42b7f9e
Sean <festizzio@gmail.com>
2022-10-05 07:02:41 -0700

    Add code to repeat a given string n times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, numberOfTimes) {
+    if(numberOfTimes < 0) {
+        return "ERROR";
+    }
+    let result = "";
+    for(; numberOfTimes > 0; numberOfTimes--) {
+        result += word;
+    }
+    return result;
 };
 
 // Do not edit below this line

3ae6c871af16b3063f6f7fc9244a2774736b6594
Sean <festizzio@gmail.com>
2022-10-05 07:02:06 -0700

    Add code to reverse a string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(reverseMe) {
+    let reversed = "";
+    let arr = reverseMe.split("");
+    for(let i = arr.length - 1; i >= 0; i--) {
+        reversed += (arr[i]);
+    }
+    return reversed;
 };
 
 // Do not edit below this line

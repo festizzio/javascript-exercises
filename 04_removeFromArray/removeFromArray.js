2ec79d7386914819812f2f0c3a602137624e88f6
Sean <festizzio@gmail.com>
2022-10-05 07:01:32 -0700

    Add code to remove a character or array of characters from an existing array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...removeFromArray) {
+    // Copying the array to not modify the original
+    let resultArray = [...array];
+    for(const value of removeFromArray) {
+        let start = resultArray.indexOf(value);
+        if(start >= 0) {
+            resultArray.splice(start, 1);
+        }
+    }
+    return resultArray;
 };
 
 // Do not edit below this line

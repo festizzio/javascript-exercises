d1a67ecaedc6f692906981af6d81a3d34761965a
Sean <festizzio@gmail.com>
2022-10-05 06:58:48 -0700

    Add code to check if a string is a palindrome, excluding white space and punctuation

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (potentialPalindrome) {
+    // Check if string is the same forwards and backwards
+    // ignore punctuation and white space
+    // change to lower case before comparing
+    
+    let comparisonString = potentialPalindrome.toLowerCase().
+                            replace(/\W/g, "");
 
+    const len = comparisonString.length;
+    for(let i = 0, j = len - 1; i < len / 2; i++, j--) {
+        if(!(comparisonString[i] === comparisonString[j])) {
+            return false;
+        }
+    }
+
+    return true;
 };
 
 // Do not edit below this line

b24b7c5858d016eecd8b910bb0767bc9a3e9eb7e
Sean <festizzio@gmail.com>
2022-10-05 07:24:49 -0700

    Add code for obtaining the nth fibonacci number using dynamic programming

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if(n < 0) {
+        return "OOPS";
+    }
+    if(!n.typeof !== Number) {
+        n = +n;
+    }
+    let fib = [1, 1];
+    let result = 1;
 
+    for(let i = 2; i < n; i++) {
+        result = fib[0] + fib[1];
+        fib[0] = fib[1];
+        fib[1] = result;
+    }
+
+    return result;
 };
 
 // Do not edit below this line

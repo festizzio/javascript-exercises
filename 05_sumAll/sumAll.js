7dfc39e3f7c41d6eee5d515386a5e936ec0e5c25
Sean <festizzio@gmail.com>
2022-10-05 07:00:33 -0700

    Add code to sum all of the numbers between two positive integers, inclusively

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    if(typeof(num1) !== "number" || num1 < 0) {
+        num1 = NaN;
+    }
+    if(typeof(num2) !== "number" || num2 < 0) {
+        num2 = NaN;
+    }
+    if(!(num1) || !(num2)) {
+        return "ERROR";
+    }
 
+    let start = Math.min(num1, num2), end = Math.max(num1, num2);
+
+    for(let i = start; start <= end; start++) {
+        sum += start;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line

7eb77d22f377068cbc13a3fcdf95e25fd6346a92
Sean <festizzio@gmail.com>
2022-10-06 07:57:46 -0700

    Add code to shift characters in string containing only letter characters

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,23 @@
-const caesar = function() {
+const caesar = function(originalString, shiftValue) {
+    let result = "";
+    // Get the charcode at a specific index, then add the shiftValue to it, finally
+    // convert it back to a char and append to result
 
+    for(let i = 0; i < originalString.length; i++) {
+        if(originalString[i].match(/\W/)) {
+            result += originalString[i];
+            continue;
+        }
+        let code = originalString.charCodeAt(i);
+        if(code >= 65 && code <= 90) {
+            
+        }
+        let shiftedCode = code + shiftValue;
+
+        result += String.fromCharCode(shiftedCode);
+    }
+
+    return result;
 };
 
 // Do not edit below this line

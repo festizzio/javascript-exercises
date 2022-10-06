61ecce8e5df92aeb1ff9362eed4ee56e1f81c4db
Sean <festizzio@gmail.com>
2022-10-06 07:17:15 -0700

    Add code to check for oldest age in input array

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(inputArray) {
+    let maxAge = 0;
+    let maxAgeIndex = 0;
+    for(let i = 0; i < inputArray.length - 1; i++) {
+        let currentAge = getAgeOf(inputArray[i]);
+        let nextAge = getAgeOf(inputArray[i + 1]);
+        if(currentAge < nextAge) {
+            maxAge = nextAge;
+            maxAgeIndex = i + 1;
+        }
+    }
 
+    return inputArray[maxAgeIndex];
 };
+
+function getAgeOf(person) {
+    if(!person.yearOfDeath) {
+        let today = new Date();
+        return today.getFullYear() - person.yearOfBirth;
+    }
+    return person.yearOfDeath - person.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
